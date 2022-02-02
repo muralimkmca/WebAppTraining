3495bb6a7a863a6dedbb0fbd47352a57919ede98
Murali Krishnamurthy <muralimk@outlook.com>
2022-02-02 23:31:57 +0530

    Add project files.

---
+++ WebAppTraining/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebAppTraining.Default" Theme="Theme1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtContent" runat="server"></asp:TextBox>
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+            </div>
+        <asp:ListBox ID="ListBox1" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged">
+            <asp:ListItem>Theme1</asp:ListItem>
+            <asp:ListItem>Theme2</asp:ListItem>
+        </asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ WebAppTraining/Default.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppTraining
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        static string themeValue;
+
+        public Default()
+        {
+            this.PreInit += Default_PreInit;   
+        }
+
+        private void Default_PreInit(object sender, EventArgs e)
+        {
+            Page.Theme = themeValue;
+        }
+
+        //protected void Page_PreInit(object sender,EventArgs e)
+        //{
+        //    // dynamic css changes (themes)
+        //    Page.Theme = themeValue;
+        //}
+
+        protected void Page_InitComplete(object sender,EventArgs e)
+        {
+
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Hi");
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            var textValue = txtContent.Text;
+            Response.Write("<script>alert('"+ textValue+ "')</script>");
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            themeValue = ListBox1.SelectedItem.Value;
+            Response.Redirect(Request.Url.ToString());
+        }
+    }
+}
